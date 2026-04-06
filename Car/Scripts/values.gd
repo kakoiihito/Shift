9433ab837abf0b1800a748250964e792d69d7aa8
KakoiiHito <kakoiihito@outlook.com>
2026-04-06 17:17:02 -0400

    it works... i have pacejka wa with load sensitivity and its g function limiter working flawlesssly.

--- Car/Scripts/values.gd
+++ Car/Scripts/values.gd
@@ -9,8 +9,8 @@ extends Node
 var car_value_tres: Resource
 
 
-var wheel_base = 4.0
-var track = 2.0
+var wheel_base = 2.3
+var track = 1.5
 
 	########################
 	# SUSPENSION VARIABLES #
@@ -44,7 +44,7 @@ var max_brake_torque = 20000000000000000000000000000000000.0
 	# ENGINE VARIABLES #
 	####################
 
-var max_torque = 151.0 
+var max_torque = 351.0 
 var max_rpm = 7500.0 
 var idle_rpm = 850.0
 var stall_rpm = 400.0
@@ -70,7 +70,7 @@ var drive_train_efficeny = 0.86
 var final_drive = 4.3
 var gear_ratio = [-3.968, 0.0, 3.136, 1.888, 1.330, 1.0, 0.814]
 var current_gear = 1
-var max_clutch_torque = 140.0
+var max_clutch_torque = 340.0
 var lock_threshold = 5.0
 var unlock_threshold = 12.0
 var clutch_stiffness = 64.0
@@ -80,10 +80,75 @@ var clutch_stiffness = 64.0
 	###################
 
 var wheel_radius = 0.3
-var wheel_mass = 20.0
-var rolling_resistance_coeff = 0.015
-var friction_coefficient = 1.9
-var wheel_inertia = 0.8 * wheel_mass * wheel_radius * wheel_radius
+var wheel_mass = 15.0
+var rolling_resistance_coeff = 0.013
+var friction_coefficient = 1.1
+var wheel_inertia = 0.6 * wheel_mass * wheel_radius * wheel_radius
+
+# Longitudinal
+var b0 = 1.3882
+var b1 = -8.0
+var b2 = 1150.0
+var b3 = 0.0
+var b4 = 320.0
+var b5 = 0.0
+var b6 = 0.0
+var b7 = 0.0
+var b8 = -2.0
+var b9 = 0.0
+var b10 = 0.0
+var b11 = 0.0
+var b12 = 0.0
+var b13 = 0.0
+
+# Lateral
+var a0 = 1.7379
+var a1 = -9.0
+var a2 = 1250.0
+var a3 = 1400.0
+var a4 = 6.5
+var a5 = 0.0
+var a6 = -0.08
+var a7 = -2.0
+var a8 = 0.0
+var a9 = 0.0
+var a10 = 0.0
+var a11 = 0.0
+var a12 = 0.0
+var a13 = 0.0
+var a14 = 0.0
+var a15 = 0.0
+var a16 = 0.0
+var a17 = 0.0
+
+# Longitduinal G Functions
+var rBx1 = 10.0
+var rBx2 = 8.0
+var rBx3 = 0.0
+var rCx1 = 1.05
+var rEx1 = -0.2
+var rEx2 = 0.0
+var rHx1 = 0.02
+var lambda_xalpha = 1.0
+
+# Lateral G Functions
+var rBy1 = 7.0
+var rBy2 = 2.5
+var rBy3 = 0.05
+var rBy4 = 0.0
+var rCy1 = 1.0
+var rEy1 = -0.3
+var rEy2 = 0.0
+var rHy1 = 0.02
+var rHy2 = 0.0
+var rVy1 = 0.05
+var rVy2 = -0.025
+var rVy3 = 0.0
+var rVy4 = 4.0
+var rVy5 = 1.9
+var rVy6 = 10.0
+var lambda_ykappa = 1.0
+var lambda_Vyk = 1.0
 
 	#################
 	# LSD VARIABLES #
--- Car/Scripts/wheel.gd
+++ Car/Scripts/wheel.gd
@@ -17,6 +17,68 @@ var wheel_mass = Values.wheel_mass
 var rolling_resistance_coeff = Values.rolling_resistance_coeff
 var friction_coefficient = Values.friction_coefficient
 var wheel_inertia = Values.wheel_inertia
+var camber = 0.0
+
+var b0 = Values.b0
+var b1 = Values.b1
+var b2 = Values.b2
+var b3 = Values.b3
+var b4 = Values.b4
+var b5 = Values.b5
+var b6 = Values.b6
+var b7 = Values.b7
+var b8 = Values.b8
+var b9 = Values.b9
+var b10 = Values.b10
+var b11 = Values.b11
+var b12 = Values.b12
+var b13 = Values.b13
+
+var a0 = Values.a0
+var a1 = Values.a1
+var a2 = Values.a2
+var a3 = Values.a3
+var a4 = Values.a4
+var a5 = Values.a5
+var a6 = Values.a6
+var a7 = Values.a7
+var a8 = Values.a8
+var a9 = Values.a9
+var a10 = Values.a10
+var a11 = Values.a11
+var a12 = Values.a12
+var a13 = Values.a13
+var a14 = Values.a14
+var a15 = Values.a15
+var a16 = Values.a16
+var a17 = Values.a17
+
+var rBx1 = Values.rBx1
+var rBx2 = Values.rBx2
+var rBx3 = Values.rBx3
+var rCx1 = Values.rCx1
+var rEx1 = Values.rEx1
+var rEx2 = Values.rEx2
+var rHx1 = Values.rHx1
+var lambda_xalpha = Values.lambda_xalpha
+
+var rBy1 = Values.rBy1
+var rBy2 = Values.rBy2
+var rBy3 = Values.rBy3
+var rBy4 = Values.rBy4
+var rCy1 = Values.rCy1
+var rEy1 = Values.rEy1
+var rEy2 = Values.rEy2
+var rHy1 = Values.rHy1
+var rHy2 = Values.rHy2
+var rVy1 = Values.rVy1
+var rVy2 = Values.rVy2
+var rVy3 = Values.rVy3
+var rVy4 = Values.rVy4
+var rVy5 = Values.rVy5
+var rVy6 = Values.rVy6
+var lambda_ykappa = Values.lambda_ykappa
+var lambda_Vyk = Values.lambda_Vyk
 
 func _get_point_velocity(point: Vector3) -> Vector3:
 	return car.linear_velocity + car.angular_velocity.cross(point - car.global_position)
@@ -30,61 +92,70 @@ func _get_wheel_forces(ray: RayCast3D):
 	var side_velocity = velocity_at_wheel.dot(side_dir)
 	var forward_speed = velocity_at_wheel.dot(-ray.global_transform.basis.z)
 	var wheel_surface_speed = wheel_angular_velocity[wheel_index] * wheel_radius
-	
+	var Fz = wheel_spring_force[wheel_index].length() / 1000
+		
+	if ray.is_colliding():
+		
+		var slip_angle = 0.0
+		if abs(forward_speed) > 1.0:
+			slip_angle = rad_to_deg(atan2(side_velocity, abs(forward_speed)))
+		
+		var slip_ratio: float
+		if abs(forward_speed) > 0.5:
+			slip_ratio = (wheel_surface_speed - forward_speed) / abs(forward_speed)
+		else:
+			slip_ratio = (wheel_surface_speed - forward_speed) / 0.5
+		slip_ratio = clamp(slip_ratio * 100.0, -100.0, 100.0)
+		
+		#var B = 13.6527
+		
+		var D = Fz * (b1 * Fz + b2)
+		var C = b0
+		var BCD = (b3 * pow(Fz, 2) + b4 * Fz) * exp(-b5 * Fz)
+		var B = BCD / (C * D)
+		var H = b9 * Fz + b10
+		var E = (b6* pow(Fz, 2) + b7 * Fz + b8) *  (1 - b13 * sign(slip_ratio+H))
+		var V = b11 * Fz + b12
+		var Bx1 = B * (slip_ratio + H)
+		var Fxo = D * sin(C * atan(Bx1 - E * (Bx1 - atan(Bx1)))) + V
 
-	var slip_angle = 0.0
-	if abs(forward_speed) > 10.0: 
-		slip_angle = (atan2(side_velocity, abs(forward_speed)))
-	
-	var slip_ratio: float
-	var speed_threshold = max(abs(forward_speed), abs(wheel_surface_speed), 0.5)
-	slip_ratio = clamp((wheel_surface_speed - forward_speed) / speed_threshold, -1.0, 1.0)
-	
-	
-	var B = 13.6527
-	var C = 1.7379
-	var D = friction_coefficient
-	var E = -0.5012
-	
-	#longitude_force[wheel_index] = wheel_spring_force[wheel_index].length() * D * sin(C * atan(B * slip_ratio - E *(B * slip_ratio - atan(B * slip_ratio))))
-	
-	var B1 = 11.2964
-	var C1 = 1.3882
-	var D1 = friction_coefficient
-	var E1 = -0.4809
-	
-	#lateral_force[wheel_index] = wheel_spring_force[wheel_index].length() * D1 * sin(C1 * atan(B1 * -slip_angle - E1 *(B1 * -slip_angle - atan(B1 * -slip_angle))))
-	
-	var theoretical_slip_ratio = slip_ratio / (1.0001 + abs(slip_ratio))
-	var theoretical_slip_angle = tan(clamp(slip_angle, -1.5, 1.5)) / (1.0001 + abs(slip_ratio))
-	
-	var slip_magnitude = sqrt(pow(theoretical_slip_ratio, 2) + pow(theoretical_slip_angle, 2))
-	
-	var Fx0 = (wheel_spring_force[wheel_index].length() / 2) * D * sin(C * atan(B * theoretical_slip_ratio - E *(B * theoretical_slip_ratio - atan(B * theoretical_slip_ratio))))
-	var Fy0 = (wheel_spring_force[wheel_index].length() / 2) * D1 * sin(C1 * atan(B1 * theoretical_slip_angle - E1 *(B1 * theoretical_slip_angle - atan(B1 * theoretical_slip_angle))))
-	
-	if slip_magnitude < 0.0001:
-		longitude_force[wheel_index] = 0.0
-		lateral_force[wheel_index] = 0.0
-	else:
-		longitude_force[wheel_index] = (theoretical_slip_ratio / slip_magnitude) * Fx0
-		lateral_force[wheel_index] = (theoretical_slip_angle / slip_magnitude) * Fy0
-	
-	
-	#F_max[wheel_index] = friction_coefficient * wheel_spring_force[wheel_index].length()
+		var SHxa = rHx1
+		var alpha_s = slip_angle + SHxa
+		var Bxa = (rBx1 + rBx3 * pow(camber, 2)) * cos(atan(rBx2 * slip_ratio)) * lambda_xalpha
+		var Cxa = rCx1
+		var Exa = rEx1 + rEx2 * 0
+		var Gxa0 = cos(Cxa * atan(Bxa * SHxa - Exa * (Bxa * SHxa - atan(Bxa * SHxa))))
+		var Gxa = cos(Cxa * atan(Bxa * alpha_s - Exa * (Bxa * alpha_s - atan(Bxa * alpha_s)))) / Gxa0
 
-	#var current_long_force = longitude_force[wheel_index]
-	#var current_lat_force = lateral_force[wheel_index]
+		longitude_force[wheel_index] = Gxa * Fxo
+		
+		var C1 = a0
+		var D1 = Fz * (a1 * Fz + a2) * (1 - a15 * pow(camber, 2))
+		var BCD1 = a3 * sin(atan(Fz / a4) * 2) * (1 - a5* abs(camber))
+		var B1 = BCD1 / (C1 * D1)
+		var H1 = a8 * Fz + a9 + a10 * camber
+		var E1 = (a6 * Fz + a7) * (1 - (a16 * camber + a17) * sign(-slip_angle + H1))
+		var V1 = a11 * Fz + a12 + (a13 * Fz + a14) * camber * Fz
+		var Bx2 = B1 * (-slip_angle + H1)
+		var Fyo = D1 * sin(C1 * atan(Bx2 - E1 * (Bx2 - atan(Bx2)))) + V1
 
-	#var force_2d = Vector2(current_long_force, current_lat_force)
-	#if force_2d.length() > F_max[wheel_index]:
-	#	force_2d = force_2d.normalized() * F_max[wheel_index]
-	#	longitude_force[wheel_index] = force_2d.x  
-	#	lateral_force[wheel_index] = force_2d.y  
+		var SHyk = rHy1 + rHy2 * 0
+		var kappa_s = slip_ratio + SHyk
+		var Byk = (rBy1 + rBy4 * pow(camber, 2)) * cos(atan(rBy2 * (slip_angle - rBy3))) * lambda_ykappa
+		var Cyk = rCy1
+		var Eyk = clamp(rEy1 + rEy2 * 0, -INF, 1.0)
+		var Gyk0 = cos(Cyk * atan(Byk * SHyk - Eyk * (Byk * SHyk - atan(Byk * SHyk))))
+		var Gyk = cos(Cyk * atan(Byk * kappa_s - Eyk * (Byk * kappa_s - atan(Byk * kappa_s)))) / Gyk0
 
-	var combined_force = (longitude_force[wheel_index] * -ray.global_transform.basis.z) + (lateral_force[wheel_index] * side_dir) # both vectors combined
-	var force_pos = ray.global_position - car.global_position
-	car.apply_force(combined_force , force_pos)
+		var mu_y = a2 / 1000.0
+		var DVyk = mu_y * Fz * (rVy1 + rVy2 * 0 + rVy3 * camber) * cos(atan(rVy4 * slip_angle))
+		var SVyk = DVyk * sin(rVy5 * atan(rVy6 * slip_ratio)) * lambda_Vyk
+
+		lateral_force[wheel_index] = Gyk * Fyo + SVyk
+
+		var combined_force = (longitude_force[wheel_index] * -ray.global_transform.basis.z) + (lateral_force[wheel_index] * side_dir) # both vectors combined
+		var force_pos = ray.global_position - car.global_position
+		car.apply_force(combined_force , force_pos)
 
 func _get_wheel_angular_velocity(ray: RayCast3D,delta: float):
 	var wheel_index = ray.get_meta("wheel_index") 
@@ -114,6 +185,7 @@ func _get_wheel_angular_velocity(ray: RayCast3D,delta: float):
 			
 	if abs(wheel_angular_velocity[wheel_index]) < 0.1:
 		wheel_angular_velocity[wheel_index] = 0.0
+		
 		
 		
 			
